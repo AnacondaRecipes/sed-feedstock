4c83ff19d30b10d5188f67cc9b45f0adddb0db52
Ray Donnelly <mingw.android@gmail.com>
2018-08-31 00:51:49 +0100

    Various fixes and removal of some hacks

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-./configure --prefix="$PREFIX"
-if [ $(uname -s) == 'Darwin' ]; then
-    # Modify Makefiles for gettext
-    sed -i.bak -e "s|^LDFLAGS =.*|LDFLAGS = -Wl,-rpath $PREFIX/lib|" gnulib-tests/Makefile Makefile
-else
-    # Remove test broken in sed 4.4
-    sed -i.bak -e 's|testsuite/panic-tests.sh||' Makefile
-fi
-make
-make -j 1 check
+./configure --prefix="${PREFIX}"
+# Remove test broken in sed 4.4
+sed -i.bak -e 's|testsuite/panic-tests.sh||' Makefile
+make -j${CPU_COUNT} ${VERBOSE_AT}
+make check
 make install
