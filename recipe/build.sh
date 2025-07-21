13d95dda61ff901001fa3f4b3750bd2673f7fd96
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2025-07-21 09:58:56 +0200

    address feedback

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,13 +2,6 @@
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./build-aux
 
 ./configure --prefix="${PREFIX}"
-# Remove test broken in sed 4.4
-sed -i.bak -e 's|testsuite/panic-tests.sh||' Makefile
 make -j${CPU_COUNT} ${VERBOSE_AT}
-# on arm64 some tests are failing due true ... so for now ignore those issue ...
-if [[ "${target_platform}" == osx-arm64 ]]; then
-  make check || true
-else
-  make check
-fi
+make check
 make install
